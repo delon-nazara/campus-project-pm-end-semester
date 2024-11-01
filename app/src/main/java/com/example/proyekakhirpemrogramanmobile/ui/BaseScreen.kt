1e3fc53d4613749651736cb1f5718a04a9fc22a9
delon-nazara <delon.gf.nazara@gmail.com>
2024-11-01 20:40:10 +0700

    Add base screen

---
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/ui/BaseScreen.kt
@@ -0,0 +1,59 @@
+package com.example.proyekakhirpemrogramanmobile.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyekakhirpemrogramanmobile.R
+
+@Composable
+fun BaseScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 100.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.this_is_base_screen),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp)
+            ) {
+                Text(text = stringResource(R.string.go_to_register_screen))
+            }
+            Spacer(modifier = Modifier.height(25.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp)
+            ) {
+                Text(text = stringResource(R.string.go_to_login_screen))
+            }
+        }
+    }
+}
