fa8acfab0c7a194a3c60cf65b4e6af8503fe9e22
StevenAnthony031 <steven.anthonyy92@gmail.com>
2024-11-10 23:52:16 +0700

    Template Navigasi Top Bar dan Drawer

--- app/src/main/java/com/example/proyekakhirpemrogramanmobile/NavTopBar.kt
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/NavTopBar.kt
@@ -4,21 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
@@ -30,14 +35,20 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import kotlinx.coroutines.launch
+
 
 class ProfilActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,12 +70,34 @@ fun MainContent() {
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet {
+            ModalDrawerSheet{
                 // Drawer content
-                Text(
-                    text = "Drawer Content",
-                    modifier = Modifier.padding(16.dp)
-                )
+                Column(){
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Button(
+                            onClick = {},
+
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.iconx),
+                                contentDescription = "Icon X"
+                            )
+                        }
+
+                      Spacer(modifier = Modifier.width(16.dp)) // Space between button and image
+
+                        Image(
+                            painter = painterResource(id = R.drawable.logoputih),
+                            contentDescription = "Logo Putih",
+                            modifier = Modifier.scale(0.5f),
+
+                        )
+                    }
+                }
             }
         },
     ) {
@@ -108,22 +141,25 @@ fun MainContent() {
             }
 
         ) { contentPadding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(contentPadding) // Tambahkan padding dari Scaffold
-                    .verticalScroll(rememberScrollState())
-                    .background(Color.White)
-            ) {
-                // Contoh konten scrollable
-                Text(text = "Hello World", modifier = Modifier.padding(16.dp))
-                Text(text = "This is a sample text", modifier = Modifier.padding(16.dp))
-                // Tambahkan konten lebih lanjut di sini
-            }
+            isikonten(contentPadding)
         }
     }
 }
-
+@Composable
+fun isikonten(paddingValues: PaddingValues){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+            .verticalScroll(rememberScrollState())
+            .background(Color.White)
+    ) {
+        // Contoh konten scrollable
+        Text(text = "Hello World", modifier = Modifier.padding(16.dp))
+        Text(text = "This is a sample text", modifier = Modifier.padding(16.dp))
+        // Tambahkan konten lebih lanjut di sini
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainContent() {
