6b8943bdd2948406fdfa44d85c90b2c7682c87f3
StevenAnthony031 <steven.anthonyy92@gmail.com>
2024-11-11 23:31:54 +0700

    tambahan ribbon untuk template

---
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/ProfilActivity.kt
@@ -0,0 +1,395 @@
+package com.example.proyekakhirpemrogramanmobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+
+class ProfilActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainProfil() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet{
+                //===============
+                // Drawer content
+                //===============
+                Column(
+                    modifier = Modifier.fillMaxSize().background(colorResource(R.color.very_light_blue)),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ){
+                    //===================
+                    //KONTAINER TITLE BAR
+                    //===================
+
+                    Column(
+                        modifier = Modifier.background(colorResource(R.color.very_light_blue)),
+                        verticalArrangement = Arrangement.Center
+                    ){
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween // Mengatur jarak yang sama di antara elemen
+                        ) {
+                            IconButton(
+                                onClick = {
+                                    scope.launch {
+                                        if (drawerState.isOpen) drawerState.close() else drawerState.open()
+                                    }
+                                },
+                                modifier = Modifier.size(64.dp)
+                            ) {
+                                Icon(
+                                    painter = rememberVectorPainter(image = Icons.Filled.Close),
+                                    contentDescription = "Localized description",
+                                    tint = Color.Black,
+                                    modifier = Modifier.size(48.dp)
+                                )
+                            }
+
+
+
+                            Spacer(modifier = Modifier.width(50.dp)) // Spacer tetap bisa digunakan untuk jarak khusus
+                        }
+                    }
+                    Divider(
+                        color = Color.Gray, // Warna divider, bisa disesuaikan
+                        thickness = 1.dp, // Ketebalan divider
+                        modifier = Modifier.fillMaxWidth() // Divider horizontal penuh
+                    )
+                    //===================
+                    //KONTAINER TEKS BUTTON
+                    //===================
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ){
+                        TextButton(
+                            onClick = { /* Handle click for Beranda */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .align(Alignment.Start)
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Start,
+                                modifier = Modifier.fillMaxWidth().padding(start = 10.dp)
+                            ){
+                                Text(
+                                    text = "Beranda",
+                                    color = Color.White, // Pewarnaan teks langsung
+                                )
+                            }
+                        }
+                        TextButton(
+                            onClick = { /* Handle click for Beranda */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .align(Alignment.Start)
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Start,
+                                modifier = Modifier.fillMaxWidth().padding(start = 10.dp)
+                            ){
+                                Text(
+                                    text = "Jadwal",
+                                    color = Color.White, // Pewarnaan teks langsung
+                                )
+                            }
+                        }
+                        TextButton(
+                            onClick = { /* Handle click for Beranda */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .align(Alignment.Start)
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Start,
+                                modifier = Modifier.fillMaxWidth().padding(start = 10.dp)
+                            ){
+                                Text(
+                                    text = "Mata Kuliah",
+                                    color = Color.White, // Pewarnaan teks langsung
+                                )
+                            }
+                        }
+                        TextButton(
+                            onClick = { /* Handle click for Beranda */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .align(Alignment.Start)
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Start,
+                                modifier = Modifier.fillMaxWidth().padding(start = 10.dp)
+                            ){
+                                Text(
+                                    text = "Tugas",
+                                    color = Color.White, // Pewarnaan teks langsung
+                                )
+                            }
+                        }
+                        TextButton(
+                            onClick = { /* Handle click for Beranda */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .align(Alignment.Start)
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Start,
+                                modifier = Modifier.fillMaxWidth().padding(start = 10.dp)
+                            ){
+                                Text(
+                                    text = "Modul",
+                                    color = Color.White, // Pewarnaan teks langsung
+                                )
+                            }
+                        }
+                        TextButton(
+                            onClick = { /* Handle click for Beranda */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .align(Alignment.Start)
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Start,
+                                modifier = Modifier.fillMaxWidth().padding(start = 10.dp)
+                            ){
+                                Text(
+                                    text = "Informasi",
+                                    color = Color.White, // Pewarnaan teks langsung
+                                )
+                            }
+                        }
+                        TextButton(
+                            onClick = { /* Handle click for Beranda */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .align(Alignment.Start)
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Start,
+                                modifier = Modifier.fillMaxWidth().padding(start = 10.dp)
+                            ){
+                                Text(
+                                    text = "Alat",
+                                    color = Color.White, // Pewarnaan teks langsung
+                                )
+                            }
+                        }
+                        TextButton(
+                            onClick = { /* Handle click for Beranda */ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .align(Alignment.Start)
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.Start,
+                                modifier = Modifier.fillMaxWidth().padding(start = 10.dp)
+                            ){
+                                Text(
+                                    text = "Pengaturan",
+                                    color = Color.White, // Pewarnaan teks langsung
+                                )
+                            }
+                        }
+
+
+                    }
+//                    =========================
+                    Spacer(modifier = Modifier.weight(1f))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color(0xFF001245)) // Sesuaikan warna latar belakang sesuai kebutuhan
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        // Menggunakan gambar profil dari drawable
+                        Image(
+                            painter = painterResource(id = R.drawable.profil_arkan), // Ganti dengan nama file drawable Anda
+                            contentDescription = "Profile Image",
+                            modifier = Modifier
+                                .size(50.dp)
+                                .padding(end = 20.dp)
+                                .padding(start = 10.dp)
+                        )
+
+                        Column {
+                            Text(
+                                text = "Usama Bin Laden",
+                                color = Color.White,
+                            )
+                            Text(
+                                text = "221401999",
+                                color = Color(0xFFBBBBBB),
+                            )
+                        }
+                    }
+                }
+            }
+        },
+    ) {
+        val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = Color.White,
+                        titleContentColor = colorResource(id = R.color.birulangit)
+                    ),
+                    title = {
+                        Text(
+                            text = "Central Class"
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                if (drawerState.isClosed) drawerState.open() else drawerState.close()
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.AccountCircle,
+                                contentDescription = "Account"
+                            )
+                        }
+                    },
+                    scrollBehavior = scrollBehavior
+                )
+            }
+
+        ) { contentPadding ->
+            IsiProfil(contentPadding)
+        }
+    }
+}
+@Composable
+fun IsiProfil(paddingValues: PaddingValues){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+            .verticalScroll(rememberScrollState())
+            .background(Color.White)
+    ) {
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(), // Divider horizontal penuh
+            thickness = 1.dp, // Ketebalan divider
+            color = Color.Gray // Warna divider, bisa disesuaikan
+        )
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .background(
+                        color = colorResource(R.color.dark_blue),
+                        shape = RoundedCornerShape(
+                            bottomStart = 16.dp,
+                            bottomEnd = 16.dp
+                        )
+                    )
+                    .padding(vertical = 16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Profil Anda",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMainProfil() {
+    MainProfil()
+}
--- app/src/main/java/com/example/proyekakhirpemrogramanmobile/ui/theme/Theme.kt
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/ui/theme/Theme.kt
@@ -4,12 +4,19 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.proyekakhirpemrogramanmobile.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -52,7 +59,26 @@ fun ProyekAkhirPemrogramanMobileTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
         content = content
     )
 }
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
+val AppTypography = Typography(
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    // Tambahkan gaya teks lain yang ingin Anda sesuaikan
+)
