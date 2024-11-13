ad4f0dd9a4689f827ecdf8d78b102da41f5d4856
StevenAnthony031 <steven.anthonyy92@gmail.com>
2024-11-13 21:38:01 +0700

    KelompokActivity sudah selesai

--- app/src/main/java/com/example/proyekakhirpemrogramanmobile/InformasiActivity.kt
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/InformasiActivity.kt
@@ -47,7 +47,6 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
