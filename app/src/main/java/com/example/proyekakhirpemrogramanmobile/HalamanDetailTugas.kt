8240a846615cc66283e25d298a33afab5bccaf89
fariz-arkan <arkanfariz2501@gmail.com>
2024-11-14 11:30:22 +0700

    Memisahkan activity HalamanDetailTugas dari HalamanTugas, sekarang menjadi dua halaman activity berbeda

--- app/src/main/java/com/example/proyekakhirpemrogramanmobile/HalamanDetailTugas.kt
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/HalamanDetailTugas.kt
@@ -63,7 +63,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.proyekakhirpemrogramanmobile.ui.theme.ProyekAkhirPemrogramanMobileTheme
 import kotlinx.coroutines.launch
 
 class HalamanDetailTugas : ComponentActivity() {
--- app/src/main/java/com/example/proyekakhirpemrogramanmobile/HalamanTugas.kt
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/HalamanTugas.kt
@@ -7,7 +7,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -26,8 +24,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -43,7 +39,6 @@ import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.SecondaryIndicator
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
@@ -60,7 +55,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.colorResource
@@ -469,8 +463,8 @@ fun IsiTugas(paddingValues: PaddingValues) {
         Spacer(
             modifier = Modifier.padding(15.dp)
         )
-//        KontainerTugas()
-            ListInstruksiTugas()
+        KontainerTugas()
+//            ListInstruksiTugas()
     }
 }
 //=========================
