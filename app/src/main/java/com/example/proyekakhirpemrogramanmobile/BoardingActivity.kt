335b459775337b8ca3b124218941be91625fe75d
StevenAnthony031 <steven.anthonyy92@gmail.com>
2024-11-17 23:17:08 +0700

    BoardingActivity sudah selesai

--- app/src/main/java/com/example/proyekakhirpemrogramanmobile/BoardingActivity.kt
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/BoardingActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
