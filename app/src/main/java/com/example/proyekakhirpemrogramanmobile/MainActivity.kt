3dadc1e74c12d6c9073c902f0ca98eb2f65b8c47
fariz-arkan <arkanfariz2501@gmail.com>
2024-11-18 00:11:48 +0700

    Update Halaman Pembuatan Kelas

--- app/src/main/java/com/example/proyekakhirpemrogramanmobile/MainActivity.kt
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/MainActivity.kt
@@ -34,7 +34,8 @@ class MainActivity : ComponentActivity() {
             //MainTemplate()
 //            MainHalamanTugas()
 //            LazyColumnDemo()
-            MainHalamanDetailMataKuliah()
+//            MainHalamanDetailMataKuliah()
+            MainPembuatanKelas()
 //          =====================
         }
     }
