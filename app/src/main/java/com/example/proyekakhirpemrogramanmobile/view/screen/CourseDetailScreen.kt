04ea08415d304a47df03d21dfbd3df7adc2e9321
delon-nazara <delon.gf.nazara@gmail.com>
2024-11-24 23:20:00 +0700

    Complete module detail screen

--- app/src/main/java/com/example/proyekakhirpemrogramanmobile/view/screen/CourseDetailScreen.kt
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/view/screen/CourseDetailScreen.kt
@@ -39,6 +39,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.proyekakhirpemrogramanmobile.R
+import com.example.proyekakhirpemrogramanmobile.data.generalInformationData
+import com.example.proyekakhirpemrogramanmobile.data.homeworkListData
+import com.example.proyekakhirpemrogramanmobile.data.learningModuleData
+import com.example.proyekakhirpemrogramanmobile.data.lecturerInformationData
+import com.example.proyekakhirpemrogramanmobile.data.lecturerSummaryData
 import com.example.proyekakhirpemrogramanmobile.util.Poppins
 import com.example.proyekakhirpemrogramanmobile.view.component.SideBar
 import com.example.proyekakhirpemrogramanmobile.view.component.Title
@@ -197,49 +202,3 @@ fun CourseData(
         )
     }
 }
-
-// todo: temporary data
-val generalInformationData = mapOf(
-    "Nama" to "Pemrograman Mobile",
-    "Kode" to "ILK3105",
-    "SKS" to "3 SKS",
-    "Program Studi" to "Ilmu Komputer",
-    "Fakultas" to "Ilmu Komputer dan Teknologi Informasi",
-    "Semester" to "5",
-    "Tahun" to "2024",
-    "Dosen" to "Nurrahmadayeni M.Kom",
-    "Komting" to "Muhammad Fariz Arkan",
-    "Mahasiswa" to "36 Orang",
-)
-
-val lecturerInformationData = mapOf(
-    "Hari Perkuliahan" to "Kamis",
-    "Jam Perkuliahan" to "10.30 - 13.00",
-    "Durasi Perkuliahan" to "2 Jam 30 Menit (150 Menit)",
-    "Lokasi Perkuliahan" to "Gedung D Fasilkom-TI",
-    "Ruangan Perkuliahan" to "Ruangan 104 (Lantai 1)"
-)
-
-val loremIpsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-val lecturerSummaryData = mapOf(
-    "Pertemuan 1" to loremIpsum,
-    "Pertemuan 2" to loremIpsum,
-    "Pertemuan 3" to loremIpsum,
-    "Pertemuan 4" to "-",
-    "Pertemuan 5" to "-",
-    "Pertemuan 6" to "-",
-    "Pertemuan 7" to "-",
-    "Pertemuan 8" to "-",
-)
-
-val homeworkListData = mapOf(
-    "Tugas 1" to "Membuat aplikasi kalkulator sederhana",
-    "Tugas 2" to "Presentasi jenis-jenis sistem operasi",
-    "Tugas 3" to "Proyek akhir pembuatan aplikasi mobile",
-)
-
-val learningModuleData = mapOf(
-    "Modul 1" to "Pengenalan pemrograman mobile.pptx",
-    "Modul 2" to "Instalasi android studio.pdf",
-    "Modul 3" to "Tutorial jetpack compose untuk pemula.pdf",
-)
