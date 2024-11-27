56be7f4627553e899a5a137f9ad5b19bf7736e49
delon-nazara <delon.gf.nazara@gmail.com>
2024-11-27 16:16:23 +0700

    Complete announcement screen

---
+++ app/src/main/java/com/example/proyekakhirpemrogramanmobile/view/screen/AnnouncementScreen.kt
@@ -0,0 +1,171 @@
+package com.example.proyekakhirpemrogramanmobile.view.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyekakhirpemrogramanmobile.R
+import com.example.proyekakhirpemrogramanmobile.data.listAnnouncement
+import com.example.proyekakhirpemrogramanmobile.model.AnnouncementModel
+import com.example.proyekakhirpemrogramanmobile.util.Poppins
+import com.example.proyekakhirpemrogramanmobile.view.component.SideBar
+import com.example.proyekakhirpemrogramanmobile.view.component.Title
+import com.example.proyekakhirpemrogramanmobile.view.component.TopBar
+
+@Preview
+@Composable
+fun AnnouncementScreen() {
+    val coroutineScope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val selectedMenu = R.string.sidebar_announcement
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            SideBar(
+                coroutineScope = coroutineScope,
+                drawerState = drawerState,
+                selectedMenu = selectedMenu
+            )
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopBar(
+                    coroutineScope = coroutineScope,
+                    drawerState = drawerState
+                )
+            }
+        ) { contentPadding ->
+            Column(
+                verticalArrangement = Arrangement.spacedBy(22.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(R.color.white))
+                    .padding(contentPadding)
+                    .padding(horizontal = 20.dp)
+                    .padding(bottom = 20.dp)
+            ) {
+                Title(title = stringResource(R.string.sidebar_announcement))
+                AnnouncementList()
+            }
+        }
+    }
+}
+
+@Composable
+fun AnnouncementList() {
+    if (listAnnouncement.isEmpty()) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.empty_screen_image),
+                contentDescription = "Empty screen image",
+                modifier = Modifier.width(200.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = stringResource(R.string.as_empty),
+                textAlign = TextAlign.Center,
+                fontSize = 14.sp,
+                fontFamily = Poppins,
+                fontWeight = FontWeight.SemiBold,
+                color = colorResource(R.color.gray)
+            )
+            Spacer(modifier = Modifier.height(48.dp))
+        }
+    } else {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(22.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(listAnnouncement) { announcement ->
+                AnnouncementListItem(announcement)
+            }
+        }
+    }
+}
+
+@Composable
+fun AnnouncementListItem(announcement: AnnouncementModel) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(R.color.light_blue)
+        ),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 6.dp)
+            ) {
+                Text(
+                    text = announcement.course,
+                    fontStyle = FontStyle.Italic
+                )
+                Text(
+                    text = announcement.date,
+                    fontStyle = FontStyle.Italic
+                )
+            }
+            HorizontalDivider(
+                color = colorResource(R.color.black)
+            )
+            Text(
+                text = announcement.title,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 12.dp, bottom = 6.dp)
+            )
+            Text(
+                text = announcement.description,
+                textAlign = TextAlign.Justify,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
